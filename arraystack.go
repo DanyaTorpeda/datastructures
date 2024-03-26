aaa99c8d0519ca7b3edf4b8e5bc8a9ed470164df
Daniil <umaharaji04@gmail.com>
2024-03-26 19:40:56 +0200

    initial commit

---
+++ arraystack.go
@@ -0,0 +1,36 @@
+package arraystack
+
+type Stack[T comparable] struct {
+	items []T
+}
+
+func NewStack[T comparable]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.items = append(s.items, item)
+}
+
+// поменять указатель
+func (s *Stack[T]) Pop() *T {
+	if s.IsEmpty() {
+		return nil
+	}
+
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return &item
+}
+
+func (s *Stack[T]) Peek() *T {
+	if s.IsEmpty() {
+		return nil
+	}
+
+	return &s.items[len(s.items)-1]
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
